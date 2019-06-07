65ac7cb11b4b7b7cba95e2b416a010ffc9e10642
sg <srfngrc@outlook.com>
2019-06-07 17:39:02 -0400

    1st upload

---
+++ ProgramSRFN.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamSerafin
+{
+    class ProgramSRFN
+    {
+        static void Main(string[] args)
+        {
+            //test
+        }
+    }
+}
